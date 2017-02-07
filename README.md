2a38ca9f50b7c0a1d5b9f79964e079d1b4c1e56e
MarkKoz <KozlovMark@gmail.com>
2017-02-06 17:20:44 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Chapter 09 - Quiz 01
+### Write a program which:
+1. Prompts the user for a positive integer >= 0
+2. Validates the user input to ensure it is a positive integer >= 0
+3. Allocate (dynamically) an array big enough for the data.
+4. Load the array with random numbers ranging in value from1 to 100
+5. Display the elements of the array (unsorted)
+6. Display the elements of the array (sorted)
+7. Display the average
+8. Display the median
+9. Display the mode, if none, display appropriate message
+
+### General Restrictions
+  * No global variables
+  * No infinite loops, examples include:
+      * `for(;;)`
+      * `while(1)`
+      * `while(true)`
+      * `do { // code } while(1);`
+  * No break statements to exit loops
+
+### Specific Restrictions
+  * You must use the `Starter.cpp` file and ALL the function prototypes
---
+++ src/Main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <stdlib.h> // srand, rand
+#include <time.h> // time
+
+using namespace std;
+
+/******************************************************************************
+* Declarations
+******************************************************************************/
+
+/**
+ * Ensures that the user's input is an integer >= 0.
+ *
+ * @param   userIn      the user's input string
+ * @param   userInput   the variable in which to store the user's input as an
+ *                      integer
+ */
+void validateInt(string userIn, int& userInput);
+
+/**
+ * Dynamically allocates an array of type int and returns a pointer to it.
+ *
+ * @param   size    the size of the array to be created
+ * @return          a pointer to the array created
+ */
+int* makeArray(int size);
+
+/**
+ * Loads a given array with integers of random values, ranging from 1
+ * to 100.
+ *
+ * Uses pointer arithmetic to step through the array.
+ *
+ * @param   arr     the array into which to load numbers
+ * @param   size    the size of the array
+ */
+void loadNumberData(int* arr, int size);
+
+/**
+ * Prints the contents of a given array to the console.
+ *
+ * Uses pointer arithmetic to step through the array.
+ *
+ * @param   numberData          the array of whose contents to display
+ * @param   qtyOfRandomNumbers  the size of the array
+ */
+void displayArray(int* numberData, int qtyOfRandomNumbers);
+
+/**
+ * Sorts the contents of a given array into ascending order by means of a
+ * selection sort algorithm.
+ *
+ * @param   arr     the array to sort
+ * @param   size    the size of the array
+ */
+void selectionSort(int arr[], int size);
+
+
+/**
+ * Calculates and returns the average of the values in a given array.
+ *
+ * @param   arr     the array for which to calculate the average
+ * @param   size    the size of the array
+ * @return          the average
+ */
+double average(int* arr, int size);
+
+/**
+ * Calculates and returns the median of the values in a given array.
+ *
+ * @param   arr     the array for which to calculate the median
+ * @param   size    the size of the array
+ * @return          the average
+ */
+double median(int* arr, int size);
+
+/**
+ * Calculates and returns the mode (the value which appears most frequently) of
+ * the values in a given array.
+ *
+ * Returns -1 if no value appears more than once.
+ *
+ * @param   arr     the array for which to calculate the mode
+ * @param   size    the size of the array
+ * @return          the average
+ */
+int mode(int* arr, int size);
+
+/******************************************************************************
+* Definitions
+******************************************************************************/
+
+int main() {
+	
+}
+
+void validateInt(string userIn, int& userInput) {
+
+}
+
+int* makeArray(int size) {
+	return nullptr;
+}
+
+void loadNumberData(int* arr, int size) {
+}
+
+void displayArray(int* numberData, int qtyOfRandomNumbers) {
+
+}
+
+void selectionSort(int arr[], int size) {
+
+}
+
+double average(int* arr, int num) {
+	return 0;
+}
+
+double median(int* arr, int num) {
+	return 0;
+}
+
+int mode(int* arr, int num) {
+	// TODO: Handle cases of multiple values with the same frequency?
+	return -1;
+}
