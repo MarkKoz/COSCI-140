58ad4db4eab95142dbbdad740c0edb5037460765
MarkKoz <KozlovMark@gmail.com>
2017-02-19 03:29:06 -0800

    Implemented functions to retrieve user input as a C string.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,67 @@
+#include <cctype>
+#include <cstring>
 #include <iostream>
+#include "Main.h"
 
 using namespace std;
+
+int main() {
+	char* input = getInput();
+
+	cout << "\nYour input is: ";
+	for (int i = 0; *(input + i) != '\0'; i++) {
+		cout << *(input + i);
+	}
+	cout << endl;
+	cout << "Bye" << endl;
+
+	return 0;
+}
+
+char* getInput() {
+	char* input = nullptr;
+	char c = 0;
+	int length = 2;
+
+	cout << "Enter any sequence of characters: ";
+	cin.get(c);
+
+	// TODO: Disallow whitespace as first character?
+	while (isspace(c) && c != ' ') {
+		cout << endl << "Nothing was entered. Please try again: ";
+		cin.get(c);
+	}
+
+	input = new char[length] {c, '\0'};
+	cin.get(c);
+
+	while (c != '\n') {
+		appendChar(c, input, length);
+		length++;
+		cin.get(c);
+	}
+
+/*	for (cin.get(c); c != '\n'; length++, cin.get(c)) {
+		appendChar(c, input, length);
+	}*/
+
+	cout << endl;
+
+	return input;
+}
+
+void appendChar(char c, char* cstring, int length) {
+	// Copies current string.
+	char* buffer = new char[length];
+	strcpy(buffer, cstring);
+
+	// Recreates string from copy with a size increase of 1.
+	delete [] cstring;
+	cstring = new char[length + 1];
+	strncpy(cstring, buffer, length - 1); // Doesn't copy NUL.
+	delete [] buffer;
+
+	// Puts next char into string and terminates it with NUL.
+	cstring[length - 1] = c;
+	cstring[length] = '\0';
+}
---
+++ src/Main.h
@@ -0,0 +1,20 @@
+#ifndef CH10_QUIZ_MAIN_H
+#define CH10_QUIZ_MAIN_H
+
+/**
+ * Prompts the user to input any sequence of characters and returns that input.
+ *
+ * @return              the user's input as a C string
+ */
+char* getInput();
+
+/**
+ * Appends a character to the end of a C string.
+ *
+ * @param   c           the character to be appended
+ * @param   cstring     the C string to which to append
+ * @param   length      the length of cstring including NUL
+ */
+void appendChar(char c, char* cstring, int length);
+
+#endif
