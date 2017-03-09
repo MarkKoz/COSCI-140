d7d65deeb7a9caafba47f1c1991ef3d25f07f00e
MarkKoz <KozlovMark@gmail.com>
2017-03-09 14:33:29 -0800

    Implemented function which reads the input file. Started implementing a function which parses each line of the input file.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <fstream>
 #include "Main.h"
 
-using namespace std;
+struct studentType {
+	string studentFName;
+	string studentLName;
+	int testScore;
+	char grade;
+};
 
 int main() {
+	studentType* students = nullptr;
+	int numStudents = 0;
+
+	parseFile("Data.txt", &students, &numStudents);
+
 	return 0;
+}
+
+void readFile(string fileName, string** lines, int* numLines) {
+	string line;
+	ifstream stream;
+
+	stream.open(fileName);
+
+	if (stream.fail()) {
+		cout << "\nError opening file '" << fileName << "'." << endl;
+	} else {
+		while (getline(stream, line)) {
+			string* buffer = new string[numLines];
+
+			for (int i = 0; i < *numLines; i++) {
+				buffer[i] = *lines[i];
+			}
+
+			delete [] lines;
+			numLines++;
+			*lines = new string[numLines];
+
+			for (int i = 0; i < *numLines - 1; i++) {
+				*lines[i] = buffer[i];
+			}
+
+			delete [] buffer;
+			*lines[*numLines] = line;
+		}
+	}
+}
+
+void parseFile(string fileName, studentType** students, int* numStudents) {
+	string* lines = nullptr;
+	int numLines = 0;
+
+	readFile(fileName, &lines, &numLines);
+
+	for (int i = 0; i < numLines; i++) {
+		*students[i];
+	}
 }
--- src/Main.h
+++ src/Main.h
@@ -1,4 +1,12 @@
 #ifndef CH11_QUIZ_MAIN_H
 #define CH11_QUIZ_MAIN_H
 
+#include <string>
+
+using namespace std;
+
+void readFile(string fileName, string** lines, int* numLines);
+
+void parseFile(string fileName);
+
 #endif
