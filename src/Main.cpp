a2e4266d557dd4b81710fd394d5bfb4ec6f0cfcd
MarkKoz <KozlovMark@gmail.com>
2017-05-15 18:23:05 -0700

    Edited formatting & comments in Main.cpp. Removed using directive for std. Replaced std::endl with newline character.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,288 +1,201 @@
-// Final exam
 #include <iostream>
-#include <cstring>
 #include "myStringClass.h"
-using namespace std;
 
+int main() {
+	myStringClass last, first, age; // Default constructor.
 
-int main()
-{
-	//object declaration
-	//default constructor
-	myStringClass last, first, age;
-
-	cout << "Enter your first name only\n";
-	cin >> first;//overloaded cin
+	std::cout << "Enter your first name only\n";
+	std::cin >> first; // Overloaded stream extraction operator.
 	myStringClass::validateUserInput(first);
 
-	cout << "what is your last name?\n";
-	cin >> last;//overloaded cin
+	std::cout << "what is your last name?\n";
+	std::cin >> last;
 	myStringClass::validateUserInput(last);
 
-	cout << "enter your age\n";
-	cin >> age;
+	std::cout << "enter your age\n";
+	std::cin >> age;
 	myStringClass::validateUserInt(age);
 
-	//overloaded cout
-	cout << "hello " << first << " " << last << ", age: " << age << endl;
-	
-	//object declaration
-	//overloaded constructor
-	//leftside longer and less
-	myStringClass string1 = "abx";
+	// Overloaded stream insertion operator.
+	std::cout << "hello " << first << " " << last << ", age: " << age << '\n';
+
+	myStringClass string1 = "abx"; // Overloaded constructor.
 	myStringClass string2 = "ax";
 
-	//overloaded logical operators: ==, < and >
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+	// Overloaded relational operators (equals, less than, & greater than).
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2;
+	} else if (string1 < string2) {
+		std::cout << string1 << " < " << string2;
+	} else {
+		std::cout << string1 << " > " << string2;
 	}
-	else if (string1 < string2)
-	{
-		cout << string1 << " < " << string2;
-	}
-	else
-	{
-		cout << string1 << " > " << string2;
-	}
-	cout << endl;
+	std::cout << '\n';
 
 	string1 = "axb";
 	string2 = "aa";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 < string2) {
+		std::cout << string1 << " < " << string2 << '\n';
+	} else {
+		std::cout << string1 << " > " << string2 << '\n';
 	}
-	else if (string1 < string2)
-	{
-		cout << string1 << " < " << string2;
-	}
-	else
-	{
-		cout << string1 << " > " << string2;
-	}
-	cout << endl;
 
 	string1 = "ab";
 	string2 = "axb";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
-	}
-	else if (string1 < string2)
-	{
-		cout << string1 << " < " << string2;
-	}
-	else
-	{
-		cout << string1 << " > " << string2;
-	}
-	cout << endl;
 
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 < string2) {
+		std::cout << string1 << " < " << string2 << '\n';
+	} else {
+		std::cout << string1 << " > " << string2 << '\n';
+	}
 
 	string1 = "ax";
 	string2 = "aax";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 < string2) {
+		std::cout << string1 << " < " << string2 << '\n';
+	} else {
+		std::cout << string1 << " > " << string2 << '\n';
 	}
-	else if (string1 < string2)
-	{
-		cout << string1 << " < " << string2;
-	}
-	else
-	{
-		cout << string1 << " > " << string2;
-	}
-	cout << endl;
 
 	string1 = "abcz";
 	string2 = "abc";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 < string2) {
+		std::cout << string1 << " < " << string2 << '\n';
+	} else {
+		std::cout << string1 << " > " << string2 << '\n';
 	}
-	else if (string1 < string2)
-	{
-		cout << string1 << " < " << string2;
-	}
-	else
-	{
-		cout << string1 << " > " << string2;
-	}
-	cout << endl;
 
 	string1 = "abc";
 	string2 = "abcz";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 < string2) {
+		std::cout << string1 << " < " << string2 << '\n';
+	} else {
+		std::cout << string1 << " > " << string2 << '\n';
 	}
-	else if (string1 < string2)
-	{
-		cout << string1 << " < " << string2;
-	}
-	else
-	{
-		cout << string1 << " > " << string2;
-	}
-	cout << endl;
 
 	string1 = "abc";
 	string2 = "abc";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
-	}
-	else if (string1 < string2)
-	{
-		cout << string1 << " < " << string2;
-	}
-	else
-	{
-		cout << string1 << " > " << string2;
-	}
-	cout << endl;
-	
 
-	 string1 = "abx";
-	 string2 = "ax";
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 < string2) {
+		std::cout << string1 << " < " << string2 << '\n';
+	} else {
+		std::cout << string1 << " > " << string2 << '\n';
+	}
 
-	//overloaded logical operators: ==, < and >
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+	string1 = "abx";
+	string2 = "ax";
+
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 <= string2) {
+		std::cout << string1 << " <= " << string2 << '\n';
+	} else {
+		std::cout << string1 << " >= " << string2 << '\n';
 	}
-	else if (string1 <= string2)
-	{
-		cout << string1 << " <= " << string2;
-	}
-	else
-	{
-		cout << string1 << " >= " << string2;
-	}
-	cout << endl;
 
 	string1 = "axb";
 	string2 = "aa";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 <= string2) {
+		std::cout << string1 << " <= " << string2 << '\n';
+	} else {
+		std::cout << string1 << " >= " << string2 << '\n';
 	}
-	else if (string1 <= string2)
-	{
-		cout << string1 << " <= " << string2;
-	}
-	else
-	{
-		cout << string1 << " >= " << string2;
-	}
-	cout << endl;
 
 	string1 = "ab";
 	string2 = "axb";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 <= string2) {
+		std::cout << string1 << " <= " << string2 << '\n';
+	} else {
+		std::cout << string1 << " >= " << string2 << '\n';
 	}
-	else if (string1 <= string2)
-	{
-		cout << string1 << " <= " << string2;
-	}
-	else
-	{
-		cout << string1 << " >= " << string2;
-	}
-	cout << endl;
 
 	string1 = "ax";
 	string2 = "aax";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 <= string2) {
+		std::cout << string1 << " <= " << string2 << '\n';
+	} else {
+		std::cout << string1 << " >= " << string2 << '\n';
 	}
-	else if (string1 <= string2)
-	{
-		cout << string1 << " <= " << string2;
-	}
-	else
-	{
-		cout << string1 << " >= " << string2;
-	}
-	cout << endl;
 
 	string1 = "abcz";
 	string2 = "abc";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 <= string2) {
+		std::cout << string1 << " <= " << string2 << '\n';
+	} else {
+		std::cout << string1 << " >= " << string2 << '\n';
 	}
-	else if (string1 <= string2)
-	{
-		cout << string1 << " <= " << string2;
-	}
-	else
-	{
-		cout << string1 << " >= " << string2;
-	}
-	cout << endl;
 
 	string1 = "abc";
 	string2 = "abcz";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
+
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 <= string2) {
+		std::cout << string1 << " <= " << string2 << '\n';
+	} else {
+		std::cout << string1 << " >= " << string2 << '\n';
 	}
-	else if (string1 <= string2)
-	{
-		cout << string1 << " <= " << string2;
-	}
-	else
-	{
-		cout << string1 << " >= " << string2;
-	}
-	cout << endl;
 
 	string1 = "abc";
 	string2 = "abc";
-	if (string1 == string2)
-	{
-		cout << string1 << " = " << string2;
-	}
-	else if (string1 <= string2)
-	{
-		cout << string1 << " <= " << string2;
-	}
-	else
-	{
-		cout << string1 << " >= " << string2;
-	}
-	cout << endl;
 
-	//array of myStringClass
-	myStringClass arrayOfNames[5];
+	if (string1 == string2) {
+		std::cout << string1 << " = " << string2 << '\n';
+	} else if (string1 <= string2) {
+		std::cout << string1 << " <= " << string2 << '\n';
+	} else {
+		std::cout << string1 << " >= " << string2 << '\n';
+	}
 
-	for (int x = 0; x < 5; x++)
-	{
-		cout << "enter name " << x + 1 << endl;
-		cin >> arrayOfNames[x];
+	myStringClass arrayOfNames[5]; // Array of myStringClass.
+
+	for (int x = 0; x < 5; x++) {
+		std::cout << "enter name " << x + 1 << '\n';
+		std::cin >> arrayOfNames[x];
 		myStringClass::validateUserInput(arrayOfNames[x]);
 	}
-	cout << "The names you entered are:\n";
-	for (int x = 0; x < 5; x++)
-	{
-		cout << arrayOfNames[x] << endl;
+
+	std::cout << "The names you entered are:\n";
+
+	for (int x = 0; x < 5; x++) {
+		std::cout << arrayOfNames[x] << '\n';
 	}
 
-
 	string2 = "Hello ";
-	//object declaration
-	//copy constructor
-	myStringClass string3(string2);
+	myStringClass string3(string2); // Copy constructor.
 	myStringClass string4 = string3 + "World";
-	cout << string4 << endl;
 
-	cout << "Good-bye" << endl;
+	std::cout << string4 << '\n';
+
+	std::cout << "Good-bye\n";
 	system("pause");
 
 	return 0;
